56bc48ffb25e369e7daa941a4cea90cfd05e1836
Emmaloto <icyblaze16@gmail.com>
2026-03-01 22:46:36 -0500

    Update and format time copy

--- timeFormats/insertTimes.js
+++ timeFormats/insertTimes.js
@@ -2,7 +2,7 @@
 
 // Initial Formats
 var allTimeFormats = [new TimeFormatEntry("MM_DD_YYYY", "00_00_0000", "0"),
-new TimeFormatEntry("DD_MM_YYYY", "00_00_0000", "0"), 
+new TimeFormatEntry("DD_MM_YYYY", "00_00_0000", "0"),
 new TimeFormatEntry("YYYY_MM_DD", "0000_00_00", "0"),
 new TimeFormatEntry("YY_MM_DD", "00_00_00", "0"),
 new TimeFormatEntry("DD_MM_YY", "00_00_00", "0"),
@@ -31,100 +31,108 @@ function TimeFormatEntry(format, date, time) {
 	this.format = format;
 	this.date = date;
 	this.time = time;
-	this.dateTime = function(){
+	this.dateTime = function () {
 		return this.date + "_" + this.time;
 	}
-  // this.id = 0;
-  this.copyDate = function copyToClipboard() { // w3schools.com/howto/howto_js_copy_clipboard.asp 
-  	/* Get the text field */
-  	var copyText = document.getElementById(this.format);
+	// this.id = 0;
+	this.copyDate = function copyToClipboard() { // w3schools.com/howto/howto_js_copy_clipboard.asp 
+		/* Get the text field */
+		var copyText = document.getElementById(this.format);
 
-  	/* Select the text field */
-  	copyText.select();
-  	copyText.setSelectionRange(0, 99999); /* For mobile devices */
+		/* Select the text field */
+		copyText.select();
+		copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-  	/* Copy the text inside the text field */
-  	document.execCommand("copy");
+		/* Copy the text inside the text field */
+		document.execCommand("copy");
 
-  	/* Alert the copied text */
-  	alert("Copied the text: " + copyText.value);
-  }
+		/* Alert the copied text */
+		alert("Copied the text: " + copyText.value);
+	}
 }
 
 // ---------- CHANGING HTML ----------
 
 // https://www.valentinog.com/blog/html-table/
 // Fill the HTML table with the array values
-function fillTable(table, tableData){
+function fillTable(table, tableData) {
 	table.innerHTML = '';
 
 	var counter = 0;
 	for (let formatEntry of tableData) {
 		let row = table.insertRow();
 		counter++;
-    // First field: Counter
-    let cellNum = row.insertCell();
-    let countText = document.createTextNode(counter);
-    cellNum.appendChild(countText);
+		// First field: Counter
+		let cellNum = row.insertCell();
+		let countText = document.createTextNode(counter);
+		cellNum.appendChild(countText);
 
-    // Second field: Format
-    let cellFormat = row.insertCell();
-    let formatText = document.createTextNode(formatEntry.format);
-    cellFormat.appendChild(formatText);    
+		// Second field: Format
+		let cellFormat = row.insertCell();
+		let formatText = document.createTextNode(formatEntry.format);
+		cellFormat.appendChild(formatText);
 
-    // Third field: Value
-    let cellVal = row.insertCell();
-    cellVal.innerHTML = "<b> " + formatEntry.date + " </b>"; // formatEntry.dateTime()
-    
+		// Third field: Value
+		let cellVal = row.insertCell();
+		cellVal.className = 'd-flex flex-column align-items-start';
+		cellVal.innerHTML = "<b> " + formatEntry.date + " </b>"; // formatEntry.dateTime()
 
-  }
+		// Copy button
+		let cellCopy = row.insertCell();
+		let copyButton = document.createElement('button');
+		copyButton.className = "btn btn-primary";
+		copyButton.innerHTML = "Copy";
+		copyButton.type = "button";
+		copyButton.onclick = formatEntry.copyDate;
+		cellCopy.appendChild(copyButton);
+	}
 }
 
 // ---------- SETTING TABLE DATA ----------
 
 // Set the date & time values of the format using regex
-function setFormatValues(timeFormatList){
+function setFormatValues(timeFormatList) {
 	timeFormatList.forEach(function (formatEntry, index) {
-  // https://www.w3schools.com/js/js_regexp.asp
-  // https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-  // https://www.freecodecamp.org/news/pipe-and-compose-in-javascript-5b04004ac937/
-  const monthName = selectedDate.toLocaleString('default', { month: 'long' });
-  const weekdayName = selectedDate.toLocaleString('default', { weekday: 'long' });
+		// https://www.w3schools.com/js/js_regexp.asp
+		// https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+		// https://www.freecodecamp.org/news/pipe-and-compose-in-javascript-5b04004ac937/
+		const monthName = selectedDate.toLocaleString('default', { month: 'long' });
+		const weekdayName = selectedDate.toLocaleString('default', { weekday: 'long' });
 
 
-  var dateHolder = formatEntry.format;
-  dateHolder = dateHolder.replace(/DD/g, String(selectedDate.getDate()).padStart(2, '0')); // Leading Zeroes
-  dateHolder = dateHolder.replace(/MM/g, String(selectedDate.getMonth()+1).padStart(2, '0')); // Leading Zeroes
-  dateHolder = dateHolder.replace(/YYYY/g, selectedDate.getFullYear());
+		var dateHolder = formatEntry.format;
+		dateHolder = dateHolder.replace(/DD/g, String(selectedDate.getDate()).padStart(2, '0')); // Leading Zeroes
+		dateHolder = dateHolder.replace(/MM/g, String(selectedDate.getMonth() + 1).padStart(2, '0')); // Leading Zeroes
+		dateHolder = dateHolder.replace(/YYYY/g, selectedDate.getFullYear());
 
-  dateHolder = dateHolder.replace(/D/g, selectedDate.getDate());
-  dateHolder = dateHolder.replace(/YY/g, String(selectedDate.getFullYear()).slice(2));
-  dateHolder = dateHolder.replace(/M/g, selectedDate.getMonth()+1);
+		dateHolder = dateHolder.replace(/D/g, selectedDate.getDate());
+		dateHolder = dateHolder.replace(/YY/g, String(selectedDate.getFullYear()).slice(2));
+		dateHolder = dateHolder.replace(/M/g, selectedDate.getMonth() + 1);
 
-// These will cause problems: Mon, D,  M******
-dateHolder = dateHolder.replace(/Month/g, monthName);
-  dateHolder = dateHolder.replace(/Mon/g,   monthName.slice(0, 3)); // Trim
+		// These will cause problems: Mon, D,  M******
+		dateHolder = dateHolder.replace(/Month/g, monthName);
+		dateHolder = dateHolder.replace(/Mon/g, monthName.slice(0, 3)); // Trim
 
-// Replacing the formats which were ruined by M
-// Issues with no spaces
-dateHolder = dateHolder.replace(/\d+onth/g, monthName);
-  dateHolder = dateHolder.replace(/\d+on/g,   monthName.slice(0, 3)); // Trim
+		// Replacing the formats which were ruined by M
+		// Issues with no spaces
+		dateHolder = dateHolder.replace(/\d+onth/g, monthName);
+		dateHolder = dateHolder.replace(/\d+on/g, monthName.slice(0, 3)); // Trim
 
-  dateHolder = dateHolder.replace(/Wkday/g, weekdayName);
-  dateHolder = dateHolder.replace(/Wkd/g,   weekdayName.slice(0, 3));
+		dateHolder = dateHolder.replace(/Wkday/g, weekdayName);
+		dateHolder = dateHolder.replace(/Wkd/g, weekdayName.slice(0, 3));
 
-  formatEntry.date = dateHolder;
-  formatEntry.time = selectedDate.toString().slice(15, 24);
-  // console.log(formatEntry.time);
-});
+		formatEntry.date = dateHolder;
+		formatEntry.time = selectedDate.toString().slice(15, 24);
+		// console.log(formatEntry.time);
+	});
 }
 
 
-function setDate(dateChosen){
-	if(dateChosen === "") selectedDate = new Date();
+function setDate(dateChosen) {
+	if (dateChosen === "") selectedDate = new Date();
 	else selectedDate = new Date(dateChosen);
 
-	
+
 	setFormatValues(allTimeFormats);
 	fillTable(tableInPage, allTimeFormats);
 
@@ -133,22 +141,22 @@ function setDate(dateChosen){
 
 // ---------- MANAGING SEPARATORS ----------
 // "None" has specified behaviour
-function setSeparator(){
+function setSeparator() {
 	var newDivider = "";
 	var radioButtons = document.getElementsByName("dividerRadios");
-	for(const radioBtn of radioButtons){
-		if(radioBtn.checked == true){
-            // console.log("New: " + radioBtn.value + "|| Old: "+ separator);
-            newDivider = radioBtn.value;
-          }
-        }
+	for (const radioBtn of radioButtons) {
+		if (radioBtn.checked == true) {
+			// console.log("New: " + radioBtn.value + "|| Old: "+ separator);
+			newDivider = radioBtn.value;
+		}
+	}
 
-        replaceSeparator(allTimeFormats, newDivider);
+	replaceSeparator(allTimeFormats, newDivider);
 
-      }
+}
 
-      function setCustomSeparator(){
-      	var customSep = document.getElementById("customSepInput").value;
+function setCustomSeparator() {
+	var customSep = document.getElementById("customSepInput").value;
 	// Possibly some input checks
 	// NO numbers
 
@@ -156,21 +164,21 @@ function setSeparator(){
 }
 
 
-function replaceSeparator(tableData, newDivider){
+function replaceSeparator(tableData, newDivider) {
 	var nullDivider = "<->";
 
 	// Separator might actually be null string, so reload date values with separator
-	if(separator === nullDivider) setFormatValues(allTimeFormats);
+	if (separator === nullDivider) setFormatValues(allTimeFormats);
 
 	for (let formatEntry of tableData) {
 		// console.log(formatEntry.date);
-		
+
 		// console.log(formatEntry.format);
-		if(newDivider !== ""){
-			formatEntry.date   = formatEntry.date.replaceAll(separator, newDivider);
+		if (newDivider !== "") {
+			formatEntry.date = formatEntry.date.replaceAll(separator, newDivider);
 			formatEntry.format = formatEntry.format.replaceAll(separator, newDivider);
-		}else{
-			formatEntry.date   = formatEntry.date.replaceAll(separator, newDivider);
+		} else {
+			formatEntry.date = formatEntry.date.replaceAll(separator, newDivider);
 			formatEntry.format = formatEntry.format.replaceAll(separator, nullDivider);
 		}
 	}
@@ -178,12 +186,12 @@ function replaceSeparator(tableData, newDivider){
 	fillTable(tableInPage, tableData);
 	separator = newDivider;
 
-	if(newDivider === "") separator = nullDivider;
+	if (newDivider === "") separator = nullDivider;
 
 }
 
 // Disables/enables input
-$(function(){
+$(function () {
 	// Picking dates
 	$('.datepicker').datepicker({
 		format: 'mm-dd-yyyy',
@@ -191,17 +199,17 @@ $(function(){
 		autoclose: true
 	});
 
-	$('input[name="datePicker"]').change(function() {
+	$('input[name="datePicker"]').change(function () {
 		setDate($(this).val());
 	});
 
 
 	// Picking custom divider
-	$('input:radio[name="dividerRadios"]').change(function() {
-		if ($(this).val()=='_CUSTOM_') {
+	$('input:radio[name="dividerRadios"]').change(function () {
+		if ($(this).val() == '_CUSTOM_') {
 			$("#customSepButton").attr("disabled", false);
 			$("#customSepInput").attr("disabled", false);
-		} 
+		}
 		else {
 			$("#customSepButton").attr("disabled", true);
 			$("#customSepInput").attr("disabled", true);
